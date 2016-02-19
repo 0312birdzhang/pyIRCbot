be4860d7e22b64cedbd2456a4498aea5186c700d
0312birdzhang <0312birdzhang@gmail.com>
2016-02-19 11:21:41 +0800

    修复中文乱码bug

--- function/webapi/chat.py
+++ function/webapi/chat.py
@@ -1,11 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import urllib.request
 import json
 
-API="http://127.0.0.1:10058/openxiaoice/ask?q="
+API="http://127.0.0.1:10088/openxiaoice/ask?q="
 def reply(string):
-	#调用小黄鸡api
 	try:
 		response = urllib.request.urlopen(API + urllib.request.quote(string.encode("utf8")))
 		return r.json()['answer'].encode("utf-8")
-	except:
+	except Exception as e:
+		print(e)
 		return "玩坏掉了Orz..."
+
+if __name__ == "__main__":
+	reply("Hello")
