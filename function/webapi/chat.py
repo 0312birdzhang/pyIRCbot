153c213e6b786316bbb3e7e42c5c5539d2a6b464
0312birdzhang <0312birdzhang@gmail.com>
2016-02-19 11:37:00 +0800

    fix

--- function/webapi/chat.py
+++ function/webapi/chat.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 import urllib.request
 import json
 
 API="http://127.0.0.1:10088/openxiaoice/ask?q="
+
 def reply(string):
 	try:
-		response = urllib.request.urlopen(API + urllib.request.quote(string.encode("utf8")))
-		return r.json()['answer'].encode("utf-8")
+		req = urllib.request.Request(API + urllib.request.quote(string.encode("utf8")))
+		req = urllib.request.urlopen(req).read()
+		ans = json.loads(req.decode('utf-8'))["answer"]
+		print(ans)
 	except Exception as e:
 		print(e)
 		return "玩坏掉了Orz..."
